c896547213f95ce14b8b12a5faffaf9e6ccd284b
Cris <krontroth@gmail.com>
2024-07-05 00:28:09 -0500

    edit slide main + menu 3x4

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -55,26 +55,26 @@ export class LandingComponent {
         '../../assets/img/slide/5.jpg',
       imageAlt: 'kemistry5',
     },
-    {
-      imageSrc:
-        '../../assets/img/slide/6.jpg',
-      imageAlt: 'kemistry6',
-    },
-    {
-      imageSrc:
-        '../../assets/img/slide/7.jpg',
-      imageAlt: 'kemistry7',
-    },
-    {
-      imageSrc:
-        '../../assets/img/slide/8.jpg',
-      imageAlt: 'kemistry8',
-    },
-    {
-      imageSrc:
-        '../../assets/img/slide/9.jpg',
-      imageAlt: 'kemistry9',
-    },
+    // {
+    //   imageSrc:
+    //     '../../assets/img/slide/6.jpg',
+    //   imageAlt: 'kemistry6',
+    // },
+    // {
+    //   imageSrc:
+    //     '../../assets/img/slide/7.jpg',
+    //   imageAlt: 'kemistry7',
+    // },
+    // {
+    //   imageSrc:
+    //     '../../assets/img/slide/8.jpg',
+    //   imageAlt: 'kemistry8',
+    // },
+    // {
+    //   imageSrc:
+    //     '../../assets/img/slide/9.jpg',
+    //   imageAlt: 'kemistry9',
+    // },
   ]
 
 }
