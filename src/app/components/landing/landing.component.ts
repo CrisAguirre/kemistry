037c61a1b19cc2c85f91ce079746fb690af31c0f
Cris <krontroth@gmail.com>
2024-07-14 00:45:46 -0500

    anchors top and prices

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import * as AOS from 'aos';
 
 @Component({
@@ -8,8 +9,25 @@ import * as AOS from 'aos';
 })
 
 export class LandingComponent implements OnInit {
+  constructor( private route: ActivatedRoute, private router: Router ) {}
+
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( "#" + f )
+      if ( element ) element.scrollIntoView(true)
+    });
+  }
   ngOnInit() {
     AOS.init();
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
   }
   images2 = [
     {
