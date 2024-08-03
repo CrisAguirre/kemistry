144256b6087ebf48bbc4755b109f652729025cfd
Cris <krontroth@gmail.com>
2024-08-03 11:48:39 -0500

    maquetacion hoodies pt1

--- src/app/hoodies/hoodies.component.ts
+++ src/app/hoodies/hoodies.component.ts
@@ -1,48 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-hoodies',
   templateUrl: './hoodies.component.html',
   styleUrls: ['./hoodies.component.css']
 })
-export class HoodiesComponent {
-  images3 = [
+export class HoodiesComponent  implements OnInit {
+    constructor( private route: ActivatedRoute, private router: Router ) {}
+
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( "#" + f )
+      if ( element ) element.scrollIntoView(true)
+    });
+  }
+
+  ngOnInit() {
+    AOS.init();
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
+  }
+  images6= [
     {
       imageSrc:
         '../../assets/img/hoodies/slide/1.jpg',
-      imageAlt: 'kemistry1',
+      imageAlt: 'kemistry_hoodies1',
     },
     {
       imageSrc:
         '../../assets/img/hoodies/slide/2.jpg',
-      imageAlt: 'kemistry2',
+      imageAlt: 'kemistry_hoodies2',
     },
     {
       imageSrc:
         '../../assets/img/hoodies/slide/3.jpg',
-      imageAlt: 'kemistry3',
+      imageAlt: 'kemistry_hoodies3',
     },
     {
       imageSrc:
         '../../assets/img/hoodies/slide/4.jpg',
-      imageAlt: 'kemistry4',
-    },
-    {
-      imageSrc:
-        '../../assets/img/hoodies/slide/5.jpg',
-      imageAlt: 'kemistry5',
-    },
-    {
-      imageSrc:
-        '../../assets/img/hoodies/slide/6.jpg',
-      imageAlt: 'kemistry6',
-    },
-    {
-      imageSrc:
-        '../../assets/img/hoodies/slide/7.jpg',
-      imageAlt: 'kemistry7',
+      imageAlt: 'kemistry_hoodies4',
     }
   ]
+
   images4 = [
     {
       imageSrc:
