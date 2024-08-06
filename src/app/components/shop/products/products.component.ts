42900ffcbab9a0615903f70b50aba4199fe06635
Cris <krontroth@gmail.com>
2024-08-06 16:42:17 -0500

    fix several styles

--- src/app/components/shop/products/products.component.ts
+++ src/app/components/shop/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CheckoutService } from '../../../services/checkout.service';
 import { Product } from '../../../interfaces/products.interface';
 import { ShopService } from '../../../services/products.service';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-products',
@@ -11,18 +13,29 @@ import { ShopService } from '../../../services/products.service';
 export class ProductsComponent implements OnInit {
   total$ = this.checkoutService.totalAction$;
 
+  ngOnInit() {
+    this.productsService.getProducts().subscribe(products => {
+      this.products = products;
+    });
+    AOS.init();
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
+  }
+
   products: Product[] = [];
 
   constructor(
     private productsService: ShopService,
-    private checkoutService: CheckoutService
+    private checkoutService: CheckoutService,
+    private route: ActivatedRoute, private router: Router
   ) {}
-
-  ngOnInit(): void {
-    this.productsService.getProducts().subscribe(products => {
-      this.products = products;
-    });
-  }
 
   filterProducts(category: string): void {
     this.productsService.filterProducts(category);
